f38c7c5785c692cef065d768237b81739f779e99
Patrick Koch <Patrick.Koch@avl.com>
2022-12-16 21:48:00 +0100

    Added Terraform Config for Windows VM with Security Group associated with Network Interface

---
+++ vm/win10-sg/variables.tf
@@ -0,0 +1,9 @@
+variable "my_virtual_machine_password" {
+  default     = "P@$$w0rd1234!"
+  description = "Password of the Virtual Machine"
+}
+
+variable "my_virtual_machine_size" {
+  default     = "Standard_D2_v4"
+  description = "Size of the Virtual Machine"
+}
