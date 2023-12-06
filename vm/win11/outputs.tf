bc4aa13f66b264ca3499f51af2f32e5eb85f0840
patkoch <patkoch87@gmail.com>
2023-12-06 22:27:35 +0100

    Corrected comment

--- vm/win11/outputs.tf
+++ vm/win11/outputs.tf
@@ -13,7 +13,7 @@ output "my_resource_group_location" {
   value = azurerm_resource_group.rg.location
 }
 
-# create an output var for the virtual machine public IP address
+# create an output for the virtual machine public IP address
 output "my_virtual_machine_public_ip" {
   value = azurerm_windows_virtual_machine.myvirtualmachine.public_ip_address
 }
